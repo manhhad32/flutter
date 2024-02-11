5492d471f8997a2a82e12e9b1703b3485f59c10c
Nguyen Manh Ha <manhhad32@gmail.com>
2024-02-11 17:21:35 +0700

    refactor file name

---
+++ test_drive/lib/widget/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'my_home_page.dart';
+import '../core/my_app_state.dart';
+
+class MyApp extends StatelessWidget{
+  //const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
